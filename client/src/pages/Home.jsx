2dad0f9f140fcd3a4770e160b8b051fa8dd1e9b8
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-05-20 00:58:32 +0500

    About Us section added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,11 +4,13 @@ import { MdOutlinePersonSearch } from "react-icons/md";
 import { MdOutlineHealthAndSafety } from "react-icons/md";
 import { TbRibbonHealth } from "react-icons/tb";
 import { MdOnlinePrediction } from "react-icons/md";
+import { FiArrowUpRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  useEffect(() => {
-    window.scrollTo(0, -1);
-  }, []);
+  // useEffect(() => {
+  //   window.scrollTo(0, -1);
+  // }, []);
 
   return (
     <>
@@ -18,7 +20,7 @@ const Home = () => {
             <h1 className="text-3xl font-bold py-3">
               Your Health is Our Priority Get Instant Disease Predictions
             </h1>
-            <p className="text-lg ">
+            <p className="text-lg text-gray-700 ">
               Harness the power of advanced AI to predict potential diseases
               based on your symptoms. Our system provides quick and accurate
               insights, helping you stay proactive about your health. Begin your
@@ -90,6 +92,38 @@ const Home = () => {
               remarkable accuracy.
             </p>
             <div className="absolute top-[-300px] w-full h-full opacity-20 bg-[#89c43f] duration-500 group-hover:top-0"></div>
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-gradient-to-tr from-white to-white min-h-screen px-[8%] ">
+        <div className="grid grid-cols-2 gap-4">
+          <img src={hero} alt="" />
+
+          <div className="flex px-[4%] flex-col items-start justify-center">
+            <h1 className="text-3xl font-bold py-3">About Us</h1>
+            <p className="text-md text-gray-700 ">
+              Welcome to Disease Prediction System! We're committed to
+              revolutionizing healthcare by leveraging cutting-edge technology
+              to predict potential health risks accurately. With years of
+              expertise in predictive analytics and data analysis, our dedicated
+              team is passionate about improving lives through early disease
+              detection. At Disease Prediction System, we believe in the power
+              of data-driven insights to empower individuals to take control of
+              their health. Our mission is to provide accessible and reliable
+              predictive analytics solutions, enabling proactive healthcare
+              decisions and ultimately improving health outcomes. Join us on our
+              journey to transform healthcare with innovation and compassion.
+              Together, we can make a difference in the fight against diseases.
+            </p>
+
+            <Link
+              to="/about"
+              className="bg-[#0b9444] flex gap-2 items-center my-6 mr-4 py-3 px-5 text-white font-semibold rounded-lg"
+            >
+              Read More
+              <FiArrowUpRight size={20} />
+            </Link>
           </div>
         </div>
       </section>
